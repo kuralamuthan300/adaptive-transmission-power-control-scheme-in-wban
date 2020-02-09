3cb5e249188a43ecddc64df4359ef9e11ef300f1
Kural <kuralamuthan300@gmail.com>
2020-02-09 12:21:19 +0530

    visualition is done successfully.Threshold calculation part needs to be tested to remove NaN values

--- Matlab Codes/visualizeRSSI.m
+++ Matlab Codes/visualizeRSSI.m
@@ -1,52 +1,105 @@
-Array = csvread("data.csv");
+clear;
+CSV_file = csvread("data.csv");
 
-Time  = Array(:,1);
-RSSI  = Array(:,2);
-Acc_x = Array(:,3);
-Acc_y = Array(:,4);
-Acc_z = Array(:,5);
+Time  = CSV_file(:,1);
+RSSI  = CSV_file(:,2);
+Acc_x = CSV_file(:,3);
+Acc_y = CSV_file(:,4);
+Acc_z = CSV_file(:,5);
 
 
 Total_samples = size(Time);
 Acc_Mag = zeros(Total_samples(1,1),1);
 
+
+%Calculating Accleration magnitude from Acc_X,Acc_Y,Acc_Z
 for itr = 1:Total_samples(1,1)
     sum = (Acc_x(itr,1)^2)+(Acc_y(itr,1)^2)+(Acc_z(itr,1)^2);
     Acc_Mag(itr,1) = sum^0.5;
 end
 
-
-Smooth_RSSI = sgolayfilt(Array(:,2),6,21);
+%Signal smoothening with Savitzky-Golay filtering.Polynomial order = 6 and
+%framelength = 21
+Smooth_RSSI = sgolayfilt(RSSI,6,21);
 Smooth_Acc  = sgolayfilt(Acc_Mag,6,21);
+
+
+%RSSI visualization
+figure(1)
+plot(Smooth_RSSI);
+hold on
+plot(RSSI);
+legend('After smoothening','Before Smoothening')
+title('RSSI Smoothening')
+hold off
+
+%Accleration magnitude visualization
+figure(2)
+plot(Smooth_Acc);
+hold on
+plot(Acc_Mag);
+legend('After smoothening','Before Smoothening')
+title('Acceleration magnitude Smoothening')
+hold off
+
+%Smoothened Acc and RSSI visualization
+figure(3)
+plot(Smooth_Acc);
+hold on
+plot(Smooth_RSSI);
+legend('Smoothened Acc','Smoothened RSSI')
+title('RSSI and Accleration samples')
+hold off
+
+
+%local_max_RSSI stores local maxima of Smooth_RSSI wave
 local_max_RSSI = islocalmax(Smooth_RSSI);
 
 
-
-g_Max_idx = [];
+%Calculating global Minimum for every 3s(30 data samples) 
+g_Min_idx = [];
 for itr=1:30:Total_samples(1,1)
     if(itr+29 <=Total_samples(1,1))
-        g_Max_idx = [g_Max_idx,g_min(Smooth_Acc,itr,itr+29)];
+        g_Min_idx = [g_Min_idx,g_min(Smooth_Acc,itr,itr+29)];
     else
-        g_Max_idx = [g_Max_idx,g_min(Smooth_Acc,itr,Total_samples(1,1))];
+        g_Min_idx = [g_Min_idx,g_min(Smooth_Acc,itr,Total_samples(1,1))];
     end
 end
 
-g_Max_idx = [1,g_Max_idx];
-g_Max_idx = [g_Max_idx,Total_samples(1,1)];
+%Adding first and last packets as two end points
+g_Min_idx = [1,g_Min_idx];
+g_Min_idx = [g_Min_idx,Total_samples(1,1)];
 
 
+%Stores AVG RSSI as threshold RSSI for first cycle
+g_max_size = size(g_Min_idx);
 
-Threshold = mean(Smooth_RSSI);
-
-g_max_size = size(g_Max_idx);
-
-avg_rssi = [];
+%Calculates and stores threshold RSSI for each cycle
+avg_threshold_rssi = [];
 for itr=1:1:g_max_size(1,2)-1
-    threshold_bw_idx = threshold_calc(Smooth_RSSI,local_max_RSSI,g_Max_idx(1,itr),g_Max_idx(1,itr+1));
-    avg_rssi = [avg_rssi,threshold_bw_idx];
+    threshold_bw_idx = threshold_calc(Smooth_RSSI,local_max_RSSI,g_Min_idx(1,itr),g_Min_idx(1,itr+1));
+    avg_threshold_rssi = [avg_threshold_rssi,threshold_bw_idx];
 end
+avg_threshold_rssi = [mean(Smooth_RSSI),avg_threshold_rssi];
+
+bar(avg_threshold_rssi)
 
 
+%clearing intermediate variables from the work space
+clear local_max_RSSI;
+clear g_max_size;
+clear CSV_file;
+clear Acc_x;
+clear Acc_y;
+clear Acc_z;
+clear Acc_Mag;
+clear RSSI;
+clear sum;
+clear count;
+clear itr;
+clear Time;
+clear Total_samples;
+clear threshold_bw_idx;
 
 
 % Functions
@@ -59,6 +112,7 @@ for itr=s:1:e
         index = itr;
     end
 end
+
     
 end
 
@@ -72,7 +126,9 @@ for itr=s:1:e
         sum = sum+Smooth_RSSI(itr,1);
         count = count+1;
     end
-    avg = sum/count;
 end
+avg = sum/count;
+
     
 end
+
