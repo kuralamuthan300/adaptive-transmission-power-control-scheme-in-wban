d0c212e7a3fcff4b858e996a1e4bce08c9aa42e6
Kural <kuralamuthan300@gmail.com>
2020-05-17 20:33:06 +0530

    New Threshold calculation implemented for dynamic activity

---
+++ Matlab Codes/Old Code/Activity_Recognition.m
@@ -0,0 +1,234 @@
+clear;
+%Training file
+CSV_file = preprocessor(csvread("Dataset/WithSeq/indoor1.csv"),1);
+%Test file
+CSV_file_pred = preprocessor(csvread("Dataset/WithSeq/outdoor1.csv"),2);
+
+total_samples = size(CSV_file);
+
+% numFeatures = 4;
+numHiddenUnits = 30;
+numClasses = 5;
+
+% layers = [ 
+%     sequenceInputLayer(numFeatures)
+%     lstmLayer(numHiddenUnits,'OutputMode','sequence')
+%     fullyConnectedLayer(numClasses)
+%     softmaxLayer
+%     classificationLayer];
+
+
+layers = [ ...
+    sequenceInputLayer(4)
+    lstmLayer(numHiddenUnits,'OutputMode','sequence')
+    fullyConnectedLayer(numClasses)
+    softmaxLayer
+    classificationLayer];
+
+ maxEpochs = 15;
+ miniBatchSize = 5;
+
+%  options = trainingOptions('adam', ...
+%      'ExecutionEnvironment','cpu', ...
+%      'GradientThreshold',1, ...
+%      'MaxEpochs',maxEpochs, ...
+%      'MiniBatchSize',miniBatchSize, ...
+%      'SequenceLength','longest', ...
+%      'Shuffle','never', ...
+%      'Verbose',0, ...
+%     'Plots','training-progress');
+
+options = trainingOptions('adam', ...
+    'ExecutionEnvironment','cpu', ...
+    'GradientThreshold',0.001, ...
+    'MaxEpochs',maxEpochs, ...
+    'MiniBatchSize',miniBatchSize, ...
+    'SequenceLength','longest', ...
+    'Shuffle','never', ...
+    'Verbose',0, ...
+    'Plots','training-progress');
+
+
+
+
+walking = magic(0);
+walking_up = magic(0);
+walking_down = magic(0);
+sitting = magic(0);
+standing = magic(0);
+for a = 1:total_samples(1,1)
+   row = CSV_file(a,:);
+   inertial_values=row(3:6);
+   label=row(8);
+   
+    if label == 1
+    walking = [walking;inertial_values];
+    end
+    if label == 2
+    walking_up= [walking_up;inertial_values];
+    end
+    if label == 3
+        walking_down= [walking_down;inertial_values];
+    end
+    if label == 4
+        sitting= [sitting;inertial_values];
+    end
+    if label == 5
+        standing= [standing;inertial_values];
+    end
+end
+
+clear row;
+
+Xtrain = cell(0,0);
+Xtrain = {transpose(walking);transpose(walking_up);transpose(walking_down);transpose(sitting);transpose(standing)};
+
+
+Wal_label = zeros(1,getSize(walking));
+for i = 1 : size(walking)
+  Wal_label(i) = 1;
+end
+
+Walup_label = zeros(1,getSize(walking_up));
+for i = 1 : size(walking_up)
+  Walup_label(i) = 2;
+end
+
+Waldown_label = zeros(1,getSize(walking_down));
+for i = 1 : size(walking_down)
+  Waldown_label(i) = 3;
+end
+
+sit_label = zeros(1,getSize(sitting));
+for i = 1 : size(sitting)
+  sit_label(i) = 4;
+end
+
+stand_label = zeros(1,getSize(standing));
+for i = 1 : size(standing)
+  stand_label(i) = 5;
+end
+%c = row1;
+%c = [cell; row2]
+
+
+Wal_label = categorical(Wal_label);
+Walup_label = categorical(Walup_label);
+Waldown_label = categorical(Waldown_label);
+sit_label = categorical(sit_label);
+stand_label = categorical(stand_label);
+
+Ytrain={Wal_label;Walup_label;Waldown_label;sit_label;stand_label};
+
+
+
+
+%Model training
+net = trainNetwork(Xtrain,Ytrain,layers,options);
+
+%Activity classification
+Xtest = transpose(CSV_file_pred(:,[3:6]));
+Ypred = classify( net, Xtest);
+Ypred = double(Ypred);
+
+
+
+%Saving the data samples in their corresponding matrix
+
+Walk_pred = magic(0);
+Walk_Up_pred = magic(0);
+Walk_Down_pred = magic(0);
+Sit_pred = magic(0);
+Stand_pred = magic(0);
+
+for i = 1:total_samples(1,1)
+
+    if(Ypred(i)==1)
+    Walk_pred = [Walk_pred;CSV_file_pred(i,:)];
+    end
+    if(Ypred(i)==2)
+    Walk_Up_pred = [Walk_Up_pred;CSV_file_pred(i,:)];
+    end
+    if(Ypred(i)==3)
+    Walk_Down_pred = [Walk_Down_pred;CSV_file_pred(i,:)];
+    end
+    if(Ypred(i)==4)
+    Sit_pred = [Sit_pred;CSV_file_pred(i,:)];
+    end
+    if(Ypred(i)==5)
+    Stand_pred = [Stand_pred;CSV_file_pred(i,:)];
+    end
+    
+end
+
+
+clear walking;
+clear walking_down;
+clear walking_up;
+clear sitting;
+clear standing;
+clear a;
+clear Wal_label;
+clear Waldown_label;
+clear Walup_label;
+clear sit_label;
+clear stand_label;
+clear total_samples;
+clear maxEpochs;
+clear miniBatchSize;
+clear numFeatures;
+clear numHiddenUnits;
+clear numClasses;
+clear Xtrain;
+clear Ytrain;
+clear label;
+clear i;
+clear inertial_values;
+clear CSV_file;
+clear CSV_file_pred;
+clear layers;
+clear net;
+clear options;
+clear Xtest;
+clear Ypred;
+
+
+function noofsamples = getSize(activity)
+
+s = size(activity);
+noofsamples=s(1,1);
+
+end
+
+
+function file = preprocessor(CSV_file,var)
+Acc_x = CSV_file(:,3);
+Acc_y = CSV_file(:,4);
+Acc_z = CSV_file(:,5);
+
+total_samples = size(CSV_file);
+Acc_Mag = zeros(total_samples(1,1),1);
+
+
+%Calculating Accleration magnitude from Acc_X,Acc_Y,Acc_Z
+for itr = 1:total_samples(1,1)
+    sum = (Acc_x(itr,1)^2)+(Acc_y(itr,1)^2)+(Acc_z(itr,1)^2);
+    Acc_Mag(itr,1) = sum^0.5;
+end
+
+Acc_Mag=sgolayfilt(Acc_Mag,6,21);
+
+left  = CSV_file(:,[1:2]);
+if var == 1
+right = CSV_file(:,[6:10]);
+else
+right = CSV_file(:,[6:9]);
+end
+
+
+file = left;
+file = [file Acc_Mag];
+file = [file right];
+
+
+end
---
+++ Matlab Codes/Thres.m
@@ -0,0 +1,159 @@
+clear;
+Acc_X=randi([-250,250], [2000,1]);
+Acc_Y=randi([-250,250], [2000,1]);
+Acc_Z=randi([-250,250], [2000,1]);
+RSSI=randi([20,70], [2000,1]);
+label=randi([1,3], [2000,1]);
+
+
+len = size(Acc_X);
+len = len(1,1);
+
+%Calculating Accleration magnitude from Acc_X,Acc_Y,Acc_Z
+Acc_Mag=[];
+
+for itr = 1:len
+    sum = (Acc_X(itr,1)^2)+(Acc_Y(itr,1)^2)+(Acc_Z(itr,1)^2);
+    Acc_Mag(itr,1) = sum^0.5;
+end
+
+%Acc=sgolayfilt(Acc_Mag,6,21);
+%RSSI=sgolayfilt(RSSI,6,21);
+Acc = Acc_Mag;
+%walking
+Acc_walk=[];
+RSSI_walk=[];
+Last_thres_walk=-40;
+Threshold_walk=[-40];
+Global_min_walk=[1];
+size_walk=0;
+
+%walking_up
+Acc_walk_up=[];
+RSSI_walk_up=[];
+Last_thres_walk_up=-40;
+Threshold_walk_up=[-40];
+Global_min_walk_up=[1];
+size_walk_up=0;
+
+
+
+%walking_down
+Acc_walk_down=[];
+RSSI_walk_down=[];
+Last_thres_walk_down=-40;
+Threshold_walk_down=[-40];
+Global_min_walk_down=[1];
+size_walk_down=0;
+
+
+
+
+for itr = 1:len
+    if(label(itr,1) == 1)
+        if(size_walk ~= 0)
+            size_walk = size_walk+1;
+            Acc_walk=[Acc_walk;Acc(itr,1)];
+            RSSI_walk=[RSSI_walk;RSSI(itr,1)];
+            if(mod(size_walk+1,30)== 0)
+                temp = size(Global_min_walk);
+                temp = temp(1,1);
+                prev_gmin=Global_min_walk(temp,1);
+                g_idx =  g_min(Acc_walk,prev_gmin);
+                Last_thres_walk = threshold_calc(RSSI_walk,islocalmax(RSSI_walk),prev_gmin);
+
+
+                Global_min_walk= [Global_min_walk;g_idx];
+                Threshold_walk=[Threshold_walk;Last_thres_walk];
+                clear temp;
+                clear prev_gmin;
+                clear g_idx;
+            end
+        elseif (size_walk==0)
+            size_walk = size_walk+1;
+            Acc_walk=[Acc_walk;Acc(itr,1)];
+            RSSI_walk=[RSSI_walk;RSSI(itr,1)];
+        end
+    
+    elseif(label(itr,1) == 2)
+        if(size_walk_up~=0)
+            size_walk_up = size_walk_up+1;
+            Acc_walk_up=[Acc_walk_up;Acc(itr,1)];
+            RSSI_walk_up=[RSSI_walk_up;RSSI(itr,1)];
+            if(mod(size_walk_up+1,30)== 0)
+                temp = size(Global_min_walk_up);
+                temp = temp(1,1);
+                prev_gmin=Global_min_walk_up(temp,1);
+                g_idx =  g_min(Acc_walk_up,prev_gmin);
+                Last_thres_walk_up = threshold_calc(RSSI_walk_up,islocalmax(RSSI_walk_up),prev_gmin);
+
+
+                Global_min_walk_up= [Global_min_walk_up;g_idx];
+                Threshold_walk_up=[Threshold_walk_up;Last_thres_walk_up];
+            end
+        elseif (size_walk_up==0)
+            size_walk_up = size_walk_up+1;
+            Acc_walk_up=[Acc_walk_up;Acc(itr,1)];
+            RSSI_walk_up=[RSSI_walk_up;RSSI(itr,1)];
+        end
+        
+    
+    elseif(label(itr,1) == 3)
+        if(size_walk_down~=0)
+            size_walk_down = size_walk_down+1;
+            Acc_walk_down=[Acc_walk_down;Acc(itr,1)];
+            RSSI_walk_down=[RSSI_walk_down;RSSI(itr,1)];
+            if(mod(size_walk_down+1,30)== 0)
+                temp = size(Global_min_walk_down);
+                temp = temp(1,1);
+                prev_gmin=Global_min_walk_down(temp,1);
+                g_idx =  g_min(Acc_walk_down,prev_gmin);
+                Last_thres_walk_down = threshold_calc(RSSI_walk_down,islocalmax(RSSI_walk_down),prev_gmin);
+
+
+                Global_min_walk_down= [Global_min_walk_down;g_idx];
+                Threshold_walk_down=[Threshold_walk_down;Last_thres_walk_down];
+            end
+        elseif (size_walk_down==0)
+            size_walk_down = size_walk_down+1;
+            Acc_walk_down=[Acc_walk_down;Acc(itr,1)];
+            RSSI_walk_down=[RSSI_walk_down;RSSI(itr,1)];
+        end
+    end
+
+end
+
+
+%global minimum algorithm
+function index = g_min(Array,s)
+    min = 1000;
+    arr_size = size(Array);
+    arr_size = arr_size(1,1);
+    for itr=s:1:arr_size
+        if(min>Array(itr,1))
+            min = Array(itr,1);
+            index = itr;
+        end
+    end
+end
+
+%Threshold calculation
+function avg = threshold_calc(Smooth_RSSI,local_max_RSSI,s)
+    count = 1;
+    sum = 0;
+
+    arr_size = size(Smooth_RSSI);
+    arr_size = arr_size(1,1);
+
+    for itr=s:1:arr_size
+        if(local_max_RSSI(itr,1) == 1)
+            sum = sum+Smooth_RSSI(itr,1);
+            count = count+1;
+        end
+    end
+    avg = sum/(count-1);
+    if isnan(avg)
+        avg = mean(Smooth_RSSI); 
+    end
+    
+    end
